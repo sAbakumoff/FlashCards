241c48acd51ccc415e5bd98f27b2ef05c81a85b9
Sergey Abakumoff <s.abakumoff@gmail.com>
2016-04-14 21:32:37 +0600

    added basic layout

--- scripts/components/main.js
+++ scripts/components/main.js
@@ -1,14 +1,34 @@
+var React = require('react');
 
 var AppBar = require('material-ui/lib/app-bar').default;
 var LeftNav = require('material-ui/lib/left-nav').default;
 var MenuItem = require('material-ui/lib/menus/menu-item').default;
 var RaisedButton = require('material-ui/lib/raised-button').default;
+var FlatButton = require('material-ui/lib/flat-button').default;
+var Badge = require('material-ui/lib/badge').default;
+var Toolbar = require('material-ui/lib/toolbar/toolbar').default;
+var ToolbarGroup = require('material-ui/lib/toolbar/toolbar-group').default;
+var ToolbarTitle = require('material-ui/lib/toolbar/toolbar-title').default;
 
-var React = require('react');
 
 var Main=()=>(
   <div>
-    <RaisedButton label="hello MUI" />
+  	<AppBar title='FlashyCards' />
+  	<LeftNav open={false}>
+  		<MenuItem>Deck 1</MenuItem>
+  		<MenuItem>Deck 2</MenuItem>
+  	</LeftNav>
+  	<Toolbar>
+  		<ToolbarGroup firstChild={true} float="left">
+  			<ToolbarTitle text="Deck 1" />
+  			<FlatButton label='Correct : 48' primary={true} />
+  			<FlatButton label="Incorrect : 18" secondary={true} />
+  			<FlatButton label="Remaining : 18"  />
+  		</ToolbarGroup>
+  		<ToolbarGroup float="right" lastChild='true'>
+  			<RaisedButton label="Start over" primary={true} />
+  		</ToolbarGroup>
+  	</Toolbar>
   </div>
 );
 
