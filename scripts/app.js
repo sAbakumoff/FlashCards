ce079ac83a7c4303f729b63b35517081fef294df
Sergey Abakumoff <s.abakumoff@gmail.com>
2016-04-14 16:04:56 +0600

    static app structure

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+
+var Main=require('./components/main');
+
+ReactDom.render(<Main />, document.getElementById('app'));
---
+++ scripts/components/main.js
@@ -0,0 +1,15 @@
+
+var AppBar = require('material-ui/lib/app-bar').default;
+var LeftNav = require('material-ui/lib/left-nav').default;
+var MenuItem = require('material-ui/lib/menus/menu-item').default;
+var RaisedButton = require('material-ui/lib/raised-button').default;
+
+var React = require('react');
+
+var Main=()=>(
+  <div>
+    <RaisedButton label="hello MUI" />
+  </div>
+);
+
+module.exports = Main;
