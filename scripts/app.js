7cc1a0d95695f217ab0b67d4c1c3551b448086a1
Sergey Abakumoff <s.abakumoff@gmail.com>
2016-04-15 16:27:21 +0600

    completed the static layout of teh app

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,20 @@
 var React = require('react');
 var ReactDom = require('react-dom');
-
+var theme = require('material-ui/styles').LightRawTheme;
+var getMuiTheme = require('material-ui/styles').getMuiTheme;
+var ThemeProvider = require('material-ui/styles').MuiThemeProvider;
 var Main=require('./components/main');
 
-ReactDom.render(<Main />, document.getElementById('app'));
+const muiTheme = getMuiTheme(theme);
+
+class App extends React.Component{
+  render(){
+    return(
+      <ThemeProvider muiTheme={muiTheme}>
+        <Main />
+      </ThemeProvider>
+    );
+  }
+}
+
+ReactDom.render(<App />, document.getElementById('app'));
--- scripts/components/main.js
+++ scripts/components/main.js
@@ -1,34 +1,60 @@
 var React = require('react');
 
-var AppBar = require('material-ui/lib/app-bar').default;
-var LeftNav = require('material-ui/lib/left-nav').default;
-var MenuItem = require('material-ui/lib/menus/menu-item').default;
-var RaisedButton = require('material-ui/lib/raised-button').default;
-var FlatButton = require('material-ui/lib/flat-button').default;
-var Badge = require('material-ui/lib/badge').default;
-var Toolbar = require('material-ui/lib/toolbar/toolbar').default;
-var ToolbarGroup = require('material-ui/lib/toolbar/toolbar-group').default;
-var ToolbarTitle = require('material-ui/lib/toolbar/toolbar-title').default;
+var MUI = require('material-ui');
+var AppBar = MUI.AppBar;
+var LeftNav = MUI.Drawer;
+var MenuItem = MUI.MenuItem;
+var RaisedButton = MUI.RaisedButton;
+var FlatButton = MUI.FlatButton;
+var FloatingActionButton = MUI.FloatingActionButton;
+var ContentFlipToBack = require('material-ui/svg-icons').ActionFlipToBack;
+var ContentFlipToFront = require('material-ui/svg-icons').ActionFlipToFront;
+var Toolbar = MUI.Toolbar;
+var ToolbarGroup = MUI.ToolbarGroup;
+var ToolbarTitle = MUI.ToolbarTitle;
+
+var Card = MUI.Card;
+var CardActions = MUI.CardActions;
+var CardHeader = MUI.CardHeader;
+var CardMedia = MUI.CardMedia;
+var CardTitle = MUI.CardTitle;
+var CardText = MUI.CardText;
 
 
 var Main=()=>(
   <div>
-  	<AppBar title='FlashyCards' />
   	<LeftNav open={false}>
   		<MenuItem>Deck 1</MenuItem>
   		<MenuItem>Deck 2</MenuItem>
   	</LeftNav>
+    <AppBar title='FlashCards' />
   	<Toolbar>
   		<ToolbarGroup firstChild={true} float="left">
   			<ToolbarTitle text="Deck 1" />
-  			<FlatButton label='Correct : 48' primary={true} />
-  			<FlatButton label="Incorrect : 18" secondary={true} />
-  			<FlatButton label="Remaining : 18"  />
+  			<FlatButton label='Correct : 8' primary={true} />
+  			<FlatButton label="Incorrect : 16" secondary={true} />
+  			<FlatButton label="Remaining : 322"  />
   		</ToolbarGroup>
-  		<ToolbarGroup float="right" lastChild='true'>
+  		<ToolbarGroup float="right" lastChild={true}>
   			<RaisedButton label="Start over" primary={true} />
   		</ToolbarGroup>
   	</Toolbar>
+    <div className='card'>
+      <Card>
+        <CardMedia>
+          <div className='card-media'>
+          x==y [x is equal to y]<br/>x===y [x is exactly equal to y]<br/>!= [not equal to]<br/>&gt; [greater than]<br/>&lt; [less than]<br/>&gt;= [greater than or equal to]<br/>&lt;= [less than or equal to]
+          <FloatingActionButton className='btn-rotate'>
+            <ContentFlipToFront />
+          </FloatingActionButton>
+          </div>
+        </CardMedia>
+        <CardActions>
+              <RaisedButton label="Correct" primary={true}  />
+              <RaisedButton label="Incorrect" secondary={true} />
+        </CardActions>
+      </Card>
+    </div>
   </div>
 );
 
