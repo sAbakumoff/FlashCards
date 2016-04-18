6eb89c916b23dddef4299bd7c7c338e562c91798
Sergey Abakumoff <s.abakumoff@gmail.com>
2016-04-18 22:47:21 +0600

    components decomposition

--- scripts/components/main.js
+++ scripts/components/main.js
@@ -20,41 +20,55 @@ var CardMedia = MUI.CardMedia;
 var CardTitle = MUI.CardTitle;
 var CardText = MUI.CardText;
 
+var Subheader = MUI.Subheader;
+
+
+var NavigationMenu = ()=>(
+  <LeftNav open={false}>
+    <MenuItem>Deck 1</MenuItem>
+    <MenuItem>Deck 2</MenuItem>
+  </LeftNav>
+);
+
+var StatusBar = ()=>(
+  <Toolbar>
+    <ToolbarGroup firstChild={true} float="left">
+      <ToolbarTitle text="Deck 1" />
+      <FlatButton label='Correct : 8' primary={true} />
+      <FlatButton label="Incorrect : 16" secondary={true} />
+      <FlatButton label="Remaining : 322"  />
+    </ToolbarGroup>
+    <ToolbarGroup float="right">
+      <RaisedButton label="Start over" primary={true} />
+    </ToolbarGroup>
+  </Toolbar>
+);
+
+var FlashCard = ()=>(
+  <div className='card'>
+    <Card>
+      <CardMedia>
+        <div className='card-media'>
+        <div className='card-content'>x==y [x is equal to y]<br/>x===y [x is exactly equal to y]<br/>!= [not equal to]<br/>&gt; [greater than]<br/>&lt; [less than]<br/>&gt;= [greater than or equal to]<br/>&lt;= [less than or equal to]</div>
+        <FloatingActionButton className='btn-rotate'>
+          <ContentFlipToFront />
+        </FloatingActionButton>
+        </div>
+      </CardMedia>
+      <CardActions>
+            <RaisedButton label="Correct" primary={true}  />
+            <RaisedButton label="Incorrect" secondary={true} />
+      </CardActions>
+    </Card>
+  </div>
+);
 
 var Main=()=>(
   <div>
-  	<LeftNav open={false}>
-  		<MenuItem>Deck 1</MenuItem>
-  		<MenuItem>Deck 2</MenuItem>
-  	</LeftNav>
     <AppBar title='FlashCards' />
-  	<Toolbar>
-  		<ToolbarGroup firstChild={true} float="left">
-  			<ToolbarTitle text="Deck 1" />
-  			<FlatButton label='Correct : 8' primary={true} />
-  			<FlatButton label="Incorrect : 16" secondary={true} />
-  			<FlatButton label="Remaining : 322"  />
-  		</ToolbarGroup>
-  		<ToolbarGroup float="right" lastChild={true}>
-  			<RaisedButton label="Start over" primary={true} />
-  		</ToolbarGroup>
-  	</Toolbar>
-    <div className='card'>
-      <Card>
-        <CardMedia>
-          <div className='card-media'>
-          x==y [x is equal to y]<br/>x===y [x is exactly equal to y]<br/>!= [not equal to]<br/>&gt; [greater than]<br/>&lt; [less than]<br/>&gt;= [greater than or equal to]<br/>&lt;= [less than or equal to]
-          <FloatingActionButton className='btn-rotate'>
-            <ContentFlipToFront />
-          </FloatingActionButton>
-          </div>
-        </CardMedia>
-        <CardActions>
-              <RaisedButton label="Correct" primary={true}  />
-              <RaisedButton label="Incorrect" secondary={true} />
-        </CardActions>
-      </Card>
-    </div>
+    <NavigationMenu />
+    <StatusBar />
+    <FlashCard />
   </div>
 );
 
